d85b413dd465d089547c2b0c704cd2e1e42a711b
victor.park <victor.park@zenvia.com>
2023-08-15 16:04:55 -0300

    feat: indentation and decode jwt

--- custom/utils.zsh
+++ custom/utils.zsh
@@ -1,12 +1,20 @@
 # Aliases
-# - Useful Aliases
-alias xx="xclip -selection clipboard"
-alias open="xdg-open"
-alias negrep="egrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,node_modules,dist} -rin"
-nfind() {
-  find -iname "*${1}*" -not -path '*node_modules*' -not -path '*dist*'
-}
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"' # Used after a pipe to alert sound (get notified when command finished executing
+  # General Aliases
+  alias xx="xclip -selection clipboard"
+  alias open="xdg-open"
+  alias negrep="egrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,node_modules,dist} -rin"
+  nfind() {
+    find -iname "*${1}*" -not -path '*node_modules*' -not -path '*dist*'
+  }
+  ndiff() {
+    diff <( printf '%s\n' "$1" ) <( printf '%s\n' "$2" )
+  }
+  alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"' # Used after a pipe to alert sound (get notified when command finished executing
+  # Aliases for software development
+  jwtd() {
+    echo "Decoding JWT...\n"
+    jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< $(echo "$1")
+  }
 
 # Libs
 # - Node
